57d9a366194849811c390f58943f63a8e2f6245c
nurrasyid14 <nurrasyid186@gmail.com>
2025-09-28 22:29:30 +0700

    setup up for nr14_data_analyser package

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# Baca README.md untuk long description
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="govdata_analyser",
+    name="govdata_analyser",  # nama package yang di-install
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -10,9 +15,25 @@ setup(
         "scikit-learn>=1.0.0",
         "matplotlib>=3.5.0",
         "seaborn>=0.11.0",
+        "scikit-fuzzy>=0.4.2",
+        "plotly>=5.0.0"
     ],
     author="Nur Rasyid",
     author_email="nurrasyid186@gmail.com",
     description="A comprehensive library for general data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nurrasyid14/General-Data-Analyser",
+    project_urls={
+        "Bug Tracker": "https://github.com/nurrasyid14/General-Data-Analyser/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
     python_requires=">=3.7",
 )
